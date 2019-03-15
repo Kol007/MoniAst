adc15a403e0a2c00116ad5d1a69b322570c79212
Nikita Romankevich <nromankevich@gmail.com>
2019-03-15 15:22:12 +0300

    Add: build folder

---
+++ public/build/precache-manifest.96e1b0d95aca5aca6835eced92d875a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "1e3c5e13fc735d283e1e",
+    "url": "/static/js/main.9fade0c0.chunk.js"
+  },
+  {
+    "revision": "e5e8d4f40dee3a19cb5a",
+    "url": "/static/js/2.50558085.chunk.js"
+  },
+  {
+    "revision": "1e3c5e13fc735d283e1e",
+    "url": "/static/css/main.4e1b6fa5.chunk.css"
+  },
+  {
+    "revision": "e5e8d4f40dee3a19cb5a",
+    "url": "/static/css/2.cb1935d9.chunk.css"
+  },
+  {
+    "revision": "554565fb05369befec868102e0703e66",
+    "url": "/index.html"
+  }
+];
