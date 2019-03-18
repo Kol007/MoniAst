063901f1a8074f2ff1efac8ceb0fa20b96b1c4b3
Nikita Romankevich <nromankevich@gmail.com>
2019-03-18 10:11:51 +0300

    Update: build

---
+++ public/build/precache-manifest.e2a7e123d2cab26da79196332d623065.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "646e9ecfd14d75554182",
+    "url": "/static/js/main.b77b8530.chunk.js"
+  },
+  {
+    "revision": "149aa72b7979c65edc89",
+    "url": "/static/js/2.fab4ddde.chunk.js"
+  },
+  {
+    "revision": "646e9ecfd14d75554182",
+    "url": "/static/css/main.92675f34.chunk.css"
+  },
+  {
+    "revision": "149aa72b7979c65edc89",
+    "url": "/static/css/2.cb1935d9.chunk.css"
+  },
+  {
+    "revision": "43e8b7716ce36637afd4b138e58e8372",
+    "url": "/index.html"
+  }
+];
